09a1ed2e7dd12a4cdaa277197ca8d5d90977ca9d
Matheus Araújo <souzamatheus8174@gmail.com>
2022-10-10 19:41:42 -0300

    chore: create app entry points

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import React from 'react';
+
+import Header from './components/Header';
+import Dashboard from './components/Dashboard';
+import NewTransectionModal from './components/NewTransectionModal';
+
+import GlobalStyle from './styles/global';
+import { TransactionsProvider } from './hooks/useTransactions';
+
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    React.useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
 
   return (
-    <div className="App">
-      hello world
-    </div>
-  )
+    <TransactionsProvider>
+      <GlobalStyle />
+      <Header onHandleOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <NewTransectionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <Dashboard />
+    </TransactionsProvider>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,62 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import { createServer, Model } from 'miragejs';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+createServer({
+  models: {
+    transactions: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-04-12 09:08:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-05-14 09:08:00'),
+        },
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transactions');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const id = Math.floor(Math.random() * 100);
+      const data = JSON.parse(request.requestBody);
+
+      const response = schema.create('transactions', {
+        id,
+        ...data,
+        createdAt: new Date(),
+      }).attrs;
+
+      return response;
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
